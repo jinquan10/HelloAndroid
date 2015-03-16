1bfc2831d8b3b68d11439320403a7f9b8cba708f
John Zhuang <john.zhuang@JohnZ.local>
2015-03-16 14:11:48 -0700

    saf

--- app/src/main/java/jz/org/helloandroid/HelloAndroid.java
+++ app/src/main/java/jz/org/helloandroid/HelloAndroid.java
@@ -1,5 +1,8 @@
 package jz.org.helloandroid;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TableLayout;
@@ -11,6 +14,30 @@ public class HelloAndroid extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        });
 
         TableLayout tl = new TableLayout(this);
 
